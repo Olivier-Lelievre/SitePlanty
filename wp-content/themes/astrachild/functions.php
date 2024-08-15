1b7191aa6965316c3f5765cd65e601eda7818ddf
Olivier Lelièvre <ladressemaildolivier@gmail.com>
2024-08-15 11:14:05 +0200

    Page Commander OK

--- wp-content/themes/astrachild/functions.php
+++ wp-content/themes/astrachild/functions.php
@@ -7,7 +7,62 @@ function theme_enqueue_styles(){
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     // Chargement du theme.css créé dans le thème enfant
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+
+
+
+
+add_filter('wp_nav_menu_items', 'astrachild_add_admin_link', 10, 2);
+function astrachild_add_admin_link($items, $args)
+    {
+      if(is_user_logged_in())
+      {
+        $newitems = $items;
+        $newitems .= '<li class="adminlink"><a href="'. admin_url() .'">Admin</a></li>';
+      }
+
+      return $newitems;
+    }
+
+
+    
 }
+
+
+
+
+
+
+/*<?php if(is_user_logged_in()) / ?>
+	<div>
+		<div>
+			<a href="<?php echo admin_url(). ?>">Admin</a>
+		</div>
+	</div>
+<?php endif; ?>
+
+
+
+
+
+/*function add_login_logout_link($items, $args)
+{
+  if(is_user_logged_in())
+  {
+    $newitems = $items;
+    $newitems .= '<li class="logoutlink"><a title="Logout" href="'. wp_logout_url('index.php') .'">Logout</a></li>';
+  }
+  else
+  {
+    $newitems = $items;
+    $newitems .= '<li><a title="Login" href="'. wp_login_url('index.php') .'">Login</a></li>';
+  }
+
+  return $newitems;
+}
+add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2);
+
+
+
 
 
 
@@ -16,16 +71,11 @@ add_filter('wp_nav_menu_items', 'wpcours_add_login_logout_menu', 10, 2);
 function wpcours_add_login_logout_menu($items, $args) {
         ob_start();
 
-
-
 NON : mettre is user loggin avec si oui  et si non
-
-
 
         wp_loginout('index.php');
         $loginoutlink = ob_get_contents();
         ob_end_clean();
-
 
 OUI  avec modifs interieur de li
         $items .= '<li style=" float: right;">'. $loginoutlink .'</li>';
